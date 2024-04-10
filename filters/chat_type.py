0425c81f7ebf8ff88e04688498f9025a178cd23c
Kubarev01 <113851142+Kubarev01@users.noreply.github.com>
2024-04-10 13:08:59 +0300

    Update chat_type.py

--- filters/chat_type.py
+++ filters/chat_type.py
@@ -1,32 +1,20 @@
-from aiogram.filters import Filter
 from aiogram import Bot, types
-
-
-
+from aiogram.dispatcher.filters import Filter
 
 class ChatTypeFilter(Filter):
     def __init__(self, chat_types: list[str]) -> None:
         self.chat_types = chat_types
 
-    async def call(self, message: types.Message) -> bool:
+    async def check(self, message: types.Message) -> bool:
         return message.chat.type in self.chat_types
-        
-# class isAdmin(Filter):
-#     def __init__(self)->None:
-#         pass
-#
-#     async def __call__(self,message:types.Message,bot:Bot)->bool:
-#         return message.from_user.id in bot.my_admins_list
 
-class isAdmin (Filter):
-    def init (self) -> None:
+class isAdmin(Filter):
+    def __init__(self) -> None:
         pass
 
-    async def __call__ (self, event, bot: Bot) -> bool:
-        if isinstance (event, types.Message):
+    async def check(self, event, bot: Bot) -> bool:
+        if isinstance(event, types.Message):
             return event.from_user.id in bot.my_admins_list
-        elif isinstance (event, types.CallbackQuery):
+        elif isinstance(event, types.CallbackQuery):
             return event.message.from_user.id in bot.my_admins_list
         return False
-
-
