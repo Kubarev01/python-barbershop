1b0a8a7be62f420a08a0ca7a89730e99aaa2e5d9
Kubarev01 <113851142+Kubarev01@users.noreply.github.com>
2024-04-11 23:18:59 +0300

    Update chat_type.py

--- filters/chat_type.py
+++ filters/chat_type.py
@@ -5,14 +5,14 @@ class ChatTypeFilter(Filter):
     def __init__(self, chat_types: list[str]) -> None:
         self.chat_types = chat_types
 
-    async def check(self, message: types.Message) -> bool:
+    async def __call__(self, message: types.Message) -> bool:
         return message.chat.type in self.chat_types
 
 class isAdmin(Filter):
     def __init__(self) -> None:
         pass
 
-    async def check(self, event, bot: Bot) -> bool:
+    async def __call__(self, event, bot: Bot) -> bool:
         if isinstance(event, types.Message):
             return event.from_user.id in bot.my_admins_list
         elif isinstance(event, types.CallbackQuery):
